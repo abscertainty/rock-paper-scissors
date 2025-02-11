8805368f503887d697a134909d17d03649a9414e
Abscertain <alibassamaladdin@gmail.com>
2025-02-11 19:33:53 +0200

    Add a prompt that asks user to choose their weapon
    The function prompts the user to enter values between 1 and 3 and
    assigns the entered value to his choice variable for eventual
    comparison.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let choices = ['Rock', 'Paper', 'Scissors']
 let randomComputerChoice
+let promptedHumanChoice
 
 function getComputerChoice(arrayOfChoices) {
     if (arrayOfChoices.length == 0 || arrayOfChoices.length > 3) return
@@ -7,4 +8,21 @@ function getComputerChoice(arrayOfChoices) {
     randomComputerChoice = Math.floor(Math.random() * nbrOfChoices)
     return arrayOfChoices[randomComputerChoice]
 }
+
+function getHumanChoice(arrayOfChoices) {
+    while(1) {
+        promptedHumanChoice = parseInt(prompt("Enter 1, 2 or 3 to choose Rock, Paper or Scissors respectively!"))
+        if (
+            !isNaN(promptedHumanChoice) && 
+            promptedHumanChoice >= 1 && 
+            promptedHumanChoice <= 3
+        ) {
+            break
+        }
+    }
+    return promptedHumanChoice - 1
+}
+
+getHumanChoice(choices)
+
 
